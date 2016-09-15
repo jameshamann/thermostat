30fb524dd31bc9baf5b1a024a0a4feecdc7e84fb
TomStuart92 <Thomascstuart@gmail.com>
2016-09-15 15:34:50 +0100

    Complete API Backend

--- Thermostat/app.rb
+++ Thermostat/app.rb
@@ -1,5 +1,6 @@
 ENV['RACK_ENV'] = 'development'
 require 'sinatra/base'
+require 'json'
 require_relative 'data_mapper_setup.rb'
 
 class App < Sinatra::Base
@@ -7,6 +8,15 @@ set :public_folder, File.dirname(__FILE__) + '/public'
 
   get '/' do
     erb :thermostat
+  end
+
+  post '/thermostat' do
+    Thermostat.create(appid: params[:appid], temperature: params[:temperature], mode: params[:mode])
+  end
+
+  get '/thermostat' do
+    record = Thermostat.last(appid: params[:appid])
+    {temperature: record.temperature, mode: record.mode}.to_json
   end
 
   # start the server if ruby file executed directly
--- Thermostat/spec/features/thermostat_api_spec.rb
+++ Thermostat/spec/features/thermostat_api_spec.rb
@@ -1,7 +1,19 @@
 require 'spec_helper'
+require 'json'
 
-feature 'saving state via api' do
-  scenario 'saving via get request' do
-    post '/thermostat', {temperature: 18, mode: 'on'}
+describe App do
+  def app
+    described_class
+  end
+
+  it 'saves via post request' do
+    post '/thermostat', {appid: 1, temperature: 18, mode: 'on'}
+    expect(Thermostat.first.temperature).to equal(18)
+  end
+
+  it ' returns via get request' do
+    post '/thermostat', {appid: 1, temperature: 19, mode: 'on'}
+    get '/thermostat', {appid: 1}
+    expect(last_response.body).to eq({temperature: 19, mode: 'on'}.to_json)
   end
 end
